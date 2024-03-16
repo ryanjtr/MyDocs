5226943f54741a02afbe30b4db414e246c2d6acc
ryanjtr <quocdangkien@gmail.com>
2024-03-16 17:16:36 +0700

    update file sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '2024'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_copybutton']
+extensions = ['sphinx_copybutton','sphinx_rtd_theme']
 
 
 # Add any paths that contain templates here, relative to this directory.
