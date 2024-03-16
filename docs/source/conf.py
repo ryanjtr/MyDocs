c5d37404b8008794fea2762a2a96c6193104ffc2
ryanjtr <quocdangkien@gmail.com>
2024-03-16 14:30:37 +0700

    add extension sphinx_copybutton

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,8 @@ release = '2024'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = ['sphinx_copybutton']
-extensions = []
+extensions = ['sphinx_copybutton']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
