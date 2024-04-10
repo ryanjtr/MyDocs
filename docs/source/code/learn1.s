11842ef2fd8f723047282f457bae6c878beda090
ryanjtr <quocdangkien@gmail.com>
2024-04-10 20:23:50 +0700

    update

--- docs/source/code/learn1.s
+++ docs/source/code/learn1.s
@@ -1,38 +1,49 @@
-# add-two-numbers.s
-# Scott F. Kaplan -- sfkaplan@cs.amherst.edu
-# Fall 2019
-#
-# Add two numbers loaded from main memory, then store the
-# result back into main memory.
-
-# Code starts here.
-
 .section .text
-        
+            
 main:
 
-        lui     a0, %hi(x_addr)      # a0 = x_addr
-        addi    a0, a0, %lo(x_addr)  #   (performed in two halves)
-        lw      t0, 0(a0)            # t0 = *a0 = *x_addr = x
+        # t0 = array length
+        lui     a0, %hi(length)
+        addi    a0, a0, %lo(length)
+        lw      t0, 0(a0)
 
-        lui     a1, %hi(y_addr)      # a1 = y_addr
-        addi    a1, a1, %lo(y_addr)  #
-        lw      t1, 0(a1)            # t1 = *a1 = *y_addr = y
-        
-        add     t2, t0, t1           # t2 = t0 + t1
-        lui     a2, %hi(z_addr)      # a2 = z_addr
-        addi    a2, a2, %lo(z_addr)  #
-        sw      t2, 0(a2)            # z = *z_addr = *a2 = t2
+        # a1 = array base
+        lui     a1, %hi(array)
+        addi    a1, a1, %lo(array)
+
+        # Loop through the array, storing the max value so far in t1.
+        # WRITE THIS LOOP
+        lw      t1, 0(a1)          # assumed t1 = max
+        li      t3, 1              # index to compare with length
+    loop:
+        addi    a1, a1, 4          # array address + 4 byte
+        lw      t4, 0(a1) 
+        # Compare and update max if satisfied
+        bge     t1, t4, not_update 
+        mv      t1, t4
+    not_update:                                                 
+        addi    t3, t3, 1
+        blt     t3, t0, loop
+    exit:
+        # Copy the result (the max value) into the result space.
+        lui     a2, %hi(result)
+        addi    a2, a2, %lo(result)
+        sw      t1, 0(a2)
+
+        # End the program by returning from main()
         jr      ra                   # return
-        
-        
-# Data is specified here.
 
+# Data is specified here.
 .section .data
-        
-x_addr:
-        .word 13                     # x = 13
-y_addr:
-        .word -5                     # y = -5
-z_addr:
-        .word 0                      # z = 0
+            
+length:
+        .word 6   # The length of the array
+array:
+    .word 3   # The values of the array
+    .word 12
+    .word 37
+    .word 15
+    .word 8
+    .word 20
+result:
+    .word 0   # The place to store the result
