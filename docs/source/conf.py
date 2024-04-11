22e2c67cd90fc4c0beb789d5f5dae81ac4a20a03
ryanjtr <quocdangkien@gmail.com>
2024-04-11 11:19:50 +0700

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,9 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 html_logo = "_static/logo.png"
-
+html_theme_options = {
+    'password': '2102003',
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
