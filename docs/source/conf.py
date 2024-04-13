fb7d04e90c35d1039f4f60ff981ac99050cf4912
ryanjtr <quocdangkien@gmail.com>
2024-04-13 20:11:58 +0700

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,5 +54,5 @@ html_logo = "_static/logo.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-def setup(app):
-    app.add_css_file("custom.css")
+# def setup(app):
+#     app.add_css_file("custom.css")
